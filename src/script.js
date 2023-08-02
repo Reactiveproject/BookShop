5d324a111c13e684b2dbc6c7a3cdf65c7e90e65b
Reactiveproject <pavel.yakushenko@gmail.com>
2023-08-02 23:06:50 +0300

    Working with Poster block
    1. JS//Make mane of some variables and consts in
    js-code more understandable
    2. JS//Create js-code for autoscrolling posters and dots

--- src/script.js
+++ src/script.js
@@ -1,35 +1,48 @@
+// Variables & Constantes
 const posterItems = document.querySelectorAll(".poster-items__item");
 const posterDotz = document.querySelectorAll(".sliderdotz__item");
 
-/////////SLIDER
+/////////Start Slider
 
-let currentImg = 0;
+function initSlider() {
+  let currentIndex = 0;
 
-const thisPoster = (index) => {
-  posterDotz.forEach((item) =>
-    item.classList.remove("sliderdotz__item_active")
-  );
-  posterDotz[index].classList.add("sliderdotz__item_active");
+  const thisPoster = (index) => {
+    posterDotz.forEach((item) =>
+      item.classList.remove("sliderdotz__item_active")
+    );
+    posterDotz[index].classList.add("sliderdotz__item_active");
 
-  posterItems.forEach((item) =>
-    item.classList.remove("poster-items__item_active")
-  );
-  posterItems[index].classList.add("poster-items__item_active");
-};
+    posterItems.forEach((item) =>
+      item.classList.remove("poster-items__item_active")
+    );
+    posterItems[index].classList.add("poster-items__item_active");
+  };
 
-const dotzScroll = (dot, index) => {
-  dot.addEventListener("click", () => {
-    currentImg = index;
-    thisPoster(currentImg);
-  });
-};
+  const Scroll = (item, index) => {
+    item.addEventListener("click", () => {
+      currentIndex = index;
+      thisPoster(currentIndex);
+    });
+  };
 
-const autoScroll = (arrCollection) => {
-  element = 0;
-  for (let element of arrCollection) {
-    thisPoster(element);
-  }
-};
+  const autoScroll = () => {
+    let index = 0;
+    setInterval(() => {
+      thisPoster(index);
+      if (index == posterItems.length - 1) {
+        index = 0;
+      } else {
+        index++;
+      }
+    }, 3000);
+  };
 
-posterItems.forEach(dotzScroll);
-posterDotz.forEach(dotzScroll);
+  posterItems.forEach(Scroll);
+  posterDotz.forEach(Scroll);
+  autoScroll();
+}
+
+addEventListener("DOMContentLoaded", initSlider);
+
+//////// End Slider
