617cdd44abee232099f436105edbe3a76b70ec49
Reactiveproject <pavel.yakushenko@gmail.com>
2023-08-20 14:42:23 +0300

    Working with fetch JS and html
    1. Add rating stars and reviews block at html
    2. Get info about reviews and ratings from fetch
    3. Add js-code for star rating block

--- src/script.js
+++ src/script.js
@@ -11,6 +11,7 @@ const itemImages = document.querySelectorAll(".cards-item__img");
 const itemAuthor = document.querySelectorAll(".cards-item__author");
 const itemTitles = document.querySelectorAll(".cards-item__title");
 const itemStars = document.querySelectorAll(".cards-item__stars");
+const itemStarsActive = document.querySelectorAll(".cards-item__stars_active");
 const itemViews = document.querySelectorAll(".cards-item__views");
 const itemDiscs = document.querySelectorAll(".cards-item__disc");
 const itemPrices = document.querySelectorAll(".cards-item__price");
@@ -142,6 +143,15 @@ function showCards() {
         itemPrices[
           i
         ].innerHTML = `${data[i].saleInfo.retailPrice.currencyCode} ${data[i].saleInfo.retailPrice.amount}`;
+      }
+
+      if (data[i].volumeInfo.ratingsCount) {
+        itemViews[i].innerHTML = `${data[i].volumeInfo.ratingsCount} review`;
+      }
+      if (data[i].volumeInfo.averageRating) {
+        itemStarsActive[i].style.width = `${
+          data[i].volumeInfo.averageRating / 0.05
+        }%`;
       }
     }
   });
