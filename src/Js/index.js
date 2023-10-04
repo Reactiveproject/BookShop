8b6858f2ce94e348489609b33f32620910db3abd
Reactiveproject <pavel.yakushenko@gmail.com>
2023-10-04 22:03:39 +0300

    Working with ES6 modules 1. Create modules for render book cards 2. Add main function for showing cards on page

--- src/Js/index.js
+++ src/Js/index.js
@@ -2,17 +2,9 @@ import { burgerMenuForMob } from "./modules/burgermenu";
 import { scroll, autoScroll, posterDotz, posterItems } from "./modules/slider";
 import { thisBookCategoty, bookCategory } from "./modules/bookcatnav";
 import { checkCatForResp, sendRequest } from "./modules/fetch";
+import { renderData } from "./modules/renderdata";
 
 const bookCards = document.querySelectorAll(".cards-block__cards-item");
-
-const itemImages = document.querySelectorAll(".cards-item__img");
-const itemAuthor = document.querySelectorAll(".cards-item__author");
-const itemTitles = document.querySelectorAll(".cards-item__title");
-const itemStars = document.querySelectorAll(".cards-item__stars");
-const itemStarsActive = document.querySelectorAll(".cards-item__stars_active");
-const itemViews = document.querySelectorAll(".cards-item__views");
-const itemDiscs = document.querySelectorAll(".cards-item__disc");
-const itemPrices = document.querySelectorAll(".cards-item__price");
 
 const loadMoreButton = document.querySelector(".books-block__button");
 
@@ -33,9 +25,8 @@ autoScroll();
 
 ////Book sidebar category navigation
 
-let currentIndex = 0;
-
 const chooseBookCategory = (item, index) => {
+  let currentIndex;
   item.addEventListener("click", () => {
     currentIndex = index;
     thisBookCategoty(currentIndex);
@@ -47,68 +38,27 @@ const chooseBookCategory = (item, index) => {
 bookCategory.forEach(chooseBookCategory);
 
 //// Fetch response to GoogleBooks
+
 export let respStartIndex = 0;
 
 checkCatForResp();
 
 //Show info from response on cards
 
-// let loadedBookArray = [];
-
-function showCards() {
-  sendRequest().then((data) => {
-    // loadedBookArray = data.slice(0);
-    // console.log(data)
-    for (let i = 0; i < data.length; i++) {
-      itemTitles[i].innerHTML = data[i].volumeInfo.title;
-      if (!data[i].volumeInfo.authors) {
-        itemAuthor[i].innerHTML = "N/A";
-      } else if (data[i].volumeInfo.authors.length > 1) {
-        itemAuthor[i].innerHTML = data[i].volumeInfo.authors.toString();
-      } else {
-        itemAuthor[i].innerHTML = data[i].volumeInfo.authors[0];
-      }
-      if (!data[i].volumeInfo.imageLinks) {
-        itemImages[
-          i
-        ].innerHTML = `<img src="/src/img/placeholder_pic.jpg" alt="pic of book" />`;
-      } else {
-        itemImages[
-          i
-        ].innerHTML = `<img src="${data[i].volumeInfo.imageLinks.thumbnail}" alt="pic of book" />`;
-      }
-      if (!data[i].volumeInfo.description) {
-        itemDiscs[i].innerHTML = "N/A";
-      } else {
-        itemDiscs[i].innerHTML = data[i].volumeInfo.description;
-      }
-      if (data[i].saleInfo.retailPrice) {
-        itemPrices[
-          i
-        ].innerHTML = `${data[i].saleInfo.retailPrice.currencyCode} ${data[i].saleInfo.retailPrice.amount}`;
-      }
-
-      if (data[i].volumeInfo.ratingsCount) {
-        itemViews[i].innerHTML = `${data[i].volumeInfo.ratingsCount} review`;
-      }
-      if (data[i].volumeInfo.averageRating) {
-        itemStarsActive[i].style.width = `${
-          data[i].volumeInfo.averageRating / 0.05
-        }%`;
-      } else {
-      }
-    }
-  });
+async function showCards() {
+  let booksArray = await sendRequest();
+  console.log(booksArray);
+  await renderData(booksArray);
 }
 
-const showMoreCards = () => {
+showCards();
+
+function showMoreCards() {
   loadMoreButton.addEventListener("click", () => {
     respStartIndex += 6;
     showCards();
   });
-};
-
-showCards();
+}
 
 showMoreCards();
 
---
+++ src/Js/modules/renderdata.js
@@ -0,0 +1,52 @@
+const itemImages = document.querySelectorAll(".cards-item__img");
+const itemAuthor = document.querySelectorAll(".cards-item__author");
+const itemTitles = document.querySelectorAll(".cards-item__title");
+const itemStars = document.querySelectorAll(".cards-item__stars");
+const itemStarsActive = document.querySelectorAll(".cards-item__stars_active");
+const itemViews = document.querySelectorAll(".cards-item__views");
+const itemDiscs = document.querySelectorAll(".cards-item__disc");
+const itemPrices = document.querySelectorAll(".cards-item__price");
+
+function renderData(data) {
+  for (let i = 0; i < data.length; i++) {
+    itemTitles[i].innerHTML = data[i].volumeInfo.title;
+    if (!data[i].volumeInfo.authors) {
+      itemAuthor[i].innerHTML = "N/A";
+    } else if (data[i].volumeInfo.authors.length > 1) {
+      itemAuthor[i].innerHTML = data[i].volumeInfo.authors.toString();
+    } else {
+      itemAuthor[i].innerHTML = data[i].volumeInfo.authors[0];
+    }
+    if (!data[i].volumeInfo.imageLinks) {
+      itemImages[
+        i
+      ].innerHTML = `<img src="/src/img/placeholder_pic.jpg" alt="pic of book" />`;
+    } else {
+      itemImages[
+        i
+      ].innerHTML = `<img src="${data[i].volumeInfo.imageLinks.thumbnail}" alt="pic of book" />`;
+    }
+    if (!data[i].volumeInfo.description) {
+      itemDiscs[i].innerHTML = "N/A";
+    } else {
+      itemDiscs[i].innerHTML = data[i].volumeInfo.description;
+    }
+    if (data[i].saleInfo.retailPrice) {
+      itemPrices[
+        i
+      ].innerHTML = `${data[i].saleInfo.retailPrice.currencyCode} ${data[i].saleInfo.retailPrice.amount}`;
+    }
+
+    if (data[i].volumeInfo.ratingsCount) {
+      itemViews[i].innerHTML = `${data[i].volumeInfo.ratingsCount} review`;
+    }
+    if (data[i].volumeInfo.averageRating) {
+      itemStarsActive[i].style.width = `${
+        data[i].volumeInfo.averageRating / 0.05
+      }%`;
+    } else {
+    }
+  }
+}
+
+export { renderData };
