69c1c157225b840cdcc03e83b0b23d429e7faa78
Reactiveproject <pavel.yakushenko@gmail.com>
2023-10-10 23:22:40 +0300

    Working with webpack 1. Add minification for js css and html in prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 let path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const PugPlugin = require("pug-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/Js/index.js"),
@@ -15,10 +18,16 @@ module.exports = {
       template: path.resolve(__dirname, "./src/index.html"), // шаблон
       filename: "index.html",
     }),
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    }),
+    new MiniCssExtractPlugin(),
+    // new PugPlugin(),
   ],
+  optimization: {
+    minimizer: [
+      // Следующая строка нужна для того, чтобы настройки optimization.minimizer добавились к настройкам по умолчанию, а не переписывали их
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
   module: {
     rules: [
       {
@@ -28,6 +37,10 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /.pug$/,
+        loader: PugPlugin.loader,
       },
     ],
   },
